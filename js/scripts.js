fe3cf3aa18ba0897bc1b5826b7cb891fc4ed6018
hailey murto and ming wen <hailey.murto@gmail.com>
2016-02-03 19:46:08 -0800

    creates whole site

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#div1-showing").slideToggle();
+    $("#div1-hidden").slideToggle();
+  });
+  $(".clickable2").click(function() {
+    $("#div2-showing").slideToggle();
+    $("#div2-hidden").slideToggle();
+  });
+  $(".clickable3").click(function() {
+    $("#div3-showing").slideToggle();
+    $("#div3-hidden").slideToggle();
+  });
+  $(".clickable4").click(function() {
+    $("#div4-showing").slideToggle();
+    $("#div4-hidden").slideToggle();
+  });
+  $(".clickable5").click(function() {
+    $("#div5-showing").slideToggle();
+    $("#div5-hidden").slideToggle();
+  });
+});
